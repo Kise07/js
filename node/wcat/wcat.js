689ea9d32bfe71ffb3bb6b07e7f7efc7f38ad533
kise <kaiazmi1412@gmail.com>
2023-05-16 22:11:31 +0530

    :tada: `:feat: -n & -b implement started`

--- node/wcat/wcat.js
+++ node/wcat/wcat.js
@@ -1,7 +1,7 @@
 // 1) node wcat.js filepath => displays the contents of a file in terminal
 // 2) node wcat.js filepath1 filepath2 filepath3 => displays the contents of all files in terminal in concatenated foem in given order
 // 3) `node wcat.js [-n] file1 file2 file3` OR `node wcat.js [-n] file1` => no. the lines of code
-const fs = require("fs"); // fs modules
+const fs = require('fs'); // fs modules
 let input = process.argv;
 // console.log(input);
 let inputArr = process.argv.slice(2);
@@ -11,56 +11,98 @@ let filesArr = []; // Files Array here!
 let optionsArr = []; // Options Array here!
 // =======> place files path in filesArr <=======
 for (let i = 0; i < inputArr.length; i++) {
-    let firstChar = inputArr[i].charAt(0);
-    // console.log(firstChar);
-    if (firstChar == '-') {
-        optionsArr.push(inputArr[i]);
-    } else {
-        filesArr.push(inputArr[i]);
-    }
+	let firstChar = inputArr[i].charAt(0);
+	// console.log(firstChar);
+	if (firstChar == '-') {
+		optionsArr.push(inputArr[i]);
+	} else {
+		filesArr.push(inputArr[i]);
+	}
 }
 // console.log("file to be read are: " + filesArr);
 
 // =======> check if all the files are present <=======
 for (let i = 0; i < filesArr.length; i++) {
-    // Check file exit using -> [FS modules]
-    let doesExist = fs.existsSync(filesArr[i]);
-    if (!doesExist) {
-        console.log("One or more Files does not exits");
-        return;
-    }
+	// Check file exit using -> [FS modules]
+	let doesExist = fs.existsSync(filesArr[i]);
+	if (!doesExist) {
+		console.log('One or more Files does not exits');
+		return;
+	}
 }
 
 // =======> content read and appending starts <=======
-let content = "";
+let content = '';
 for (let i = 0; i < filesArr.length; i++) {
-    let fileContent = fs.readFileSync(filesArr[i]);
-    content = content + fileContent + "\n";
+	let fileContent = fs.readFileSync(filesArr[i]);
+	content = content + fileContent + '\n';
 }
 // console.log(content);
 
-let contentArr = content.split("\n");
+let contentArr = content.split('\n');
 console.table(contentArr);
 
 // =======> -s command -> checking if -s is present or not <=======
-let isSpresent = optionsArr.includes("-s");
+let isSpresent = optionsArr.includes('-s');
 // -s command instructions:
 if (isSpresent) {
-    for (let i = 1; i < contentArr.length; i++) {
-        if (contentArr[i] == "" && contentArr[i - 1] == "") {
-            contentArr[i] = null;
-        } else if (contentArr[i] == "" && contentArr[i - 1] == null) {
-            contentArr[i] = null;
-        }
-    }
-    console.table(contentArr);
-    // tempArr for storing contents of -s command
-    let tempArr = [];
-    // push everything in tempArr except null values
-    for (let i = 0; i < contentArr.length; i++) {
-        if (contentArr[i] != null) {
-            tempArr.push(contentArr[i]);
-        }
-    }
-    console.log("data after removing extra lines\n", tempArr);
+	for (let i = 1; i < contentArr.length; i++) {
+		if (contentArr[i] == '' && contentArr[i - 1] == '') {
+			contentArr[i] = null;
+		} else if (contentArr[i] == '' && contentArr[i - 1] == null) {
+			contentArr[i] = null;
+		}
+	}
+	console.table(contentArr);
+	// tempArr for storing contents of -s command
+	let tempArr = [];
+	// push everything in tempArr except null values
+	for (let i = 0; i < contentArr.length; i++) {
+		if (contentArr[i] != null) {
+			tempArr.push(contentArr[i]);
+		}
+	}
+	console.log('data after removing extra lines\n', tempArr);
+}
+
+contentArr = tempArr;
+
+// -n & -b commands features instructions
+let indexOfN = optionsArr.indexOf('-n');
+let indexOfB = optionsArr.indexOf('B');
+// if -n OR -b is not found, return -1
+
+// checking for `first come, first server` either -n OR -b
+let finalOption = ''; // empty
+
+// if -n OR -b are present here! // EdgeCase -> 1
+if (indexOfN != -1 && indexOfB != -1) {
+	if (indexOfN < indexOfB) {
+		finalOption = '-n'; // empty to -n
+	} else {
+		finalOption = '-b'; // empty to -b
+	}
+} else { // either -n OR -b is present here! // EdgeCase -> 2
+	if (indexOfN != -1) {
+		finalOption = '-n';
+	} else if (indexOfB != -1) {
+		finalOption = '-b';
+	}
+}
+
+// calling of function by evaluating finalOption
+if (finalOption == "-n") {
+    modifyContentByN();
+} else if (finalOption == "-b") {
+    modifyContentByB();
+}
+
+// -n content function
+function modifyContentByN() {
+
+}
+
+// -b content function
+function modifyContentByB() {
+
 }
