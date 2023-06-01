318c207c6037910f33bd4bb9d3a750b55c3cbeab
kise <kise@kises-MacBook-Air.local>
2023-06-01 21:42:37 +0530

    :memo: feat - `help command done!`

---
+++ node/fileOrganizer/commands/help.js
@@ -0,0 +1,18 @@
+function help() {
+    console.log(`These are some of myCLI commands used in various situations
+        1. node main.js help
+        2. node main.js tree <path>
+        3. node main.js organize <path>
+    `);
+}
+
+// function abc() {
+//     console.log("in help.js");
+// }
+
+module.exports = {
+    // key : value pair
+    help: help,
+    // haathi: help, // -> eg. multiple exports
+    // ghoda: abc,
+}
--- node/fileOrganizer/main.js
+++ node/fileOrganizer/main.js
@@ -1,16 +1,22 @@
 // entry point of my command line
-
+let helpFunc = require("./commands/help");
+// console.log(helpFunc.haathi());
 let inputArr = process.argv.slice(2);
 let command = inputArr[0];
+let path = inputArr[1];
 switch (command) {
 	case "tree":
 		// call tree function
+		console.log("tree function called and executed succesfully on path " + path);
 		break;
 	case "organize":
 		// call organize function
+		console.log("organize function called and executed succesfully on path " + path);
 		break;
 	case "help":
 		// call help function
+		helpFunc.help();
+		// console.log("help function called and executed succesfully on path " + path);
 		break;
 	default:
 		console.log("command not recognized :/");
