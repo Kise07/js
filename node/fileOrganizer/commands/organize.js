ea41289acc0c29832bd50843f84797fcafb0d030
kise <kise@kises-MacBook-Air.local>
2023-06-03 12:34:42 +0530

    :memo: organize cmd in progress!

--- node/fileOrganizer/commands/organize.js
+++ node/fileOrganizer/commands/organize.js
@@ -9,6 +9,7 @@ let types = {
 }
 
 function organize(srcPath) {
+	// 1. to check if srcPath is present
 	if (srcPath == undefined) {
 		// The process.cwd() method returns the current working directory of the Node.js process.
 		// console.log(srcPath); // -> undefined
@@ -16,6 +17,7 @@ function organize(srcPath) {
 		// console.log("source path is ", srcPath);
 	}
 
+	// 2. to create a directory -> organized_files
 	let organizedFiles = path.join(srcPath, "organized_files");
 	// console.log("organized files folder path is ", organizedFiles);
 	if (fs.existsSync(organizedFiles) == false) {
@@ -24,6 +26,19 @@ function organize(srcPath) {
 	} else {
 		console.log('folder already exists');
 	}
+
+	// 3. scan the entire srcPath(downloads folder in this case)
+	// Reads the contents of the directory. ->basically read the names of files present in directory
+	let allFiles = fs.readdirSync(srcPath);
+	console.log(allFiles);
+
+	// 4. traverse over all the files and classify them on the basis of their extensions eg.(.pdf or .mp3)
+	for (let i = 0; i < allFiles.length; i++) {
+		// let ext = allFiles[i].split(".")[1];
+		let ext = path.extname(allFiles[i]);
+		console.log(ext);
+	}
 }
 
-organize();
+let srcPath = "/Users/kise/Desktop/js/node/fileOrganizer/downloads";
+organize(srcPath);
