eae93eff6a0fff5570e1ee1eb3cd51b5109a59db
kise <kaiazmi1412@gmail.com>
2023-06-05 03:56:25 +0530

    :tada: `fileOrganizer Project ✅`

--- node/fileOrganizer/main.js
+++ node/fileOrganizer/main.js
@@ -1,26 +1,28 @@
 // entry point of my command line
-let helpFunc = require("./commands/help");
-let orgFunc = require("./commands/organize");
+let helpFunc = require('./commands/help');
+let treeFunc = require('./commands/tree');
+let orgFunc = require('./commands/organize');
 // console.log(helpFunc.haathi());
 let inputArr = process.argv.slice(2);
 let command = inputArr[0];
 let path = inputArr[1];
 switch (command) {
-	case "tree":
-		// call tree function
-		console.log("tree function called and executed succesfully on path " + path);
-		break;
-	case "organize":
-		// call organize function
-		orgFunc.organize(path);
-		// console.log("organize function called and executed succesfully on path " + path);
-		break;
-	case "help":
+	case 'help':
 		// call help function
 		helpFunc.help();
 		// console.log("help function called and executed succesfully on path " + path);
 		break;
+	case 'tree':
+		// call tree function
+		treeFunc.tree(path);
+		// console.log("tree function called and executed succesfully on path " + path);
+		break;
+	case 'organize':
+		// call organize function
+		orgFunc.organize(path);
+		// console.log("organize function called and executed succesfully on path " + path);
+		break;
 	default:
-		console.log("command not recognized :/");
+		console.log('command not recognized :/');
 		break;
 }
